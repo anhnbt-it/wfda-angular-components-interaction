7067a1d5391d2bea29b89025a50f269842663b82
anhnbt-it <anhnbt.it@gmail.com>
2021-01-22 11:42:23 +0700

    add name-card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatCardModule} from '@angular/material/card';
+
 import { AppComponent } from './app.component';
+import { NameCardComponent } from './name-card/name-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import {RouterModule} from '@angular/router';
+import { FirstComponent } from './layout/first/first.component';
+import { SecondComponent } from './layout/second/second.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NameCardComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    FirstComponent,
+    SecondComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: AppComponent
+      },
+      {
+        path: 'name-card',
+        component: NameCardComponent,
+      },
+      {
+        path: 'first-component',
+        component: FirstComponent
+      },
+      {
+        path: 'second-component',
+        component: SecondComponent
+      }
+    ]),
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
